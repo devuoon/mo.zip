792d60cf4eefcf38c4a0cb80e95984595df5e6c0
Legday <chlwodud0327@naver.com>
2024-06-14 13:49:55 +0900

    Fix: 프로젝트 민감정보 암호화

--- src/main/java/com/mozip/config/WebConfig.java
+++ src/main/java/com/mozip/config/WebConfig.java
@@ -1,18 +1,24 @@
 package com.mozip.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${file.path}")
-    private String uploadFolder;
+//    @Value("${file.path}")
+//    private String uploadFolder;
+    private final Environment env;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadFolder = env.getProperty("file.path");
+
         WebMvcConfigurer.super.addResourceHandlers(registry);
 
         registry.addResourceHandler("/upload/**") // jsp 페이지에서 "/upload/**" 이 패턴이 나오면
